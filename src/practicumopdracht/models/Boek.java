9895c9eecb512d851b50e80ea931f11a1b85cc3d
Joli-Coeur Weibolt <joli-coeur.weibolt@hva.nl>
2022-02-17 16:00:29 +0100

    Made my classes

---
+++ src/practicumopdracht/models/Boek.java
@@ -0,0 +1,10 @@
+package practicumopdracht.models;
+
+import java.time.LocalDate;
+
+public class Boek {
+    private Schrijver hoortBij;
+    private String titel;
+    private LocalDate lancering;
+    private double gemiddeldeCijfer;
+}
---
+++ src/practicumopdracht/models/Schrijver.java
@@ -0,0 +1,8 @@
+package practicumopdracht.models;
+
+public class Schrijver {
+    private String voornaam;
+    private String achternaam;
+    private int leeftijd;
+    private boolean nogActief;
+}
